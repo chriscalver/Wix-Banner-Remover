66843167b352185d5a84bd467ef2f1cf3cec95d4
chriscalver <130410868+chriscalver@users.noreply.github.com>
2023-07-19 21:59:09 -0400

    added wix favicon removal

--- main3.js
+++ main3.js
@@ -33,22 +33,62 @@
         .addEventListener('change', function() {
         var fr=new FileReader();
 
-        fr.onload=function(){
+          fr.onload = function () {
             //document.getElementById('output').textContent=fr.result;
 
-			originalFile = fr.result;			
-			let index = originalFile.search('<div id="SITE_CONTAINER">');
-			let index2 = originalFile.search('<div id="site-root" class="site-root">');
 
-			let firstHalf = originalFile.slice(0, index + 25);
-			let secondHalf = originalFile.slice(index2 + 38);
-			let newFile = firstHalf + secondHalf;
-			//document.getElementById("box1").innerHTML = index; 
-			//document.getElementById('box1').textContent = index.toString();
-			//document.getElementById('box2').textContent = firstHalf;
-			//document.getElementById('box2').textContent = secondHalf;
-			document.getElementById('textbox').textContent = newFile;
-        }
+
+
+
+            originalFile = fr.result;  // put html code into a string
+
+            let index = originalFile.search('<div id="SITE_CONTAINER">');  //finds location of beg of string
+            let index2 = originalFile.search('<div id="site-root" class="site-root">'); //finds location of string
+
+            // remove wix banner
+
+            let firstHalf = originalFile.slice(0, index + 25);   // keeps string up to and including the index search
+                                                                  // cuts off everything after the div
+            let secondHalf = originalFile.slice(index2 + 38);  // this slice has only one param
+                                                                // this retns eveything after the 'root' div
+            let newFile = firstHalf + secondHalf;
+
+            // remove wix favicon
+
+            //let index3 = originalFile.search('<link rel="icon');
+            //let index4 = originalFile.search('<link rel="shortcut icon');
+           // let index5 = originalFile.search('<link rel="apple');
+            
+            //let firstHalf2 = originalFile.slice(0, index3);
+            //let secondHalf = originalFile.slice(index3 + 38); 
+            //let insert1 = '<!-- ';
+            //let insert2 = ' -->';
+            //text = text.replaceAll("Cats","Dogs");
+            newFile = newFile.replaceAll("https://www.wix.com/favicon.ico","");
+            
+
+            //var output = [originalFile.slice(0, index3), insert, originalFile.slice(index3)].join('');
+
+           // var a = "I want apple";
+            //var b = " an";
+           // var position = 6;
+           // var output = [a.slice(0, position), b, a.slice(position)].join('');
+           // console.log(output);
+
+
+
+
+
+            //document.getElementById("box1").innerHTML = index3; 
+            //document.getElementById("box2").innerHTML = index4; 
+            //document.getElementById("box3").innerHTML = index5; 
+
+
+            //document.getElementById('box1').textContent = index.toString();
+            //document.getElementById('box2').textContent = firstHalf;
+            //document.getElementById('box2').textContent = secondHalf;
+            document.getElementById('textbox').textContent = newFile;
+          }
         fr.readAsText(this.files[0]);
         })
       //document.getElementById('box1').innerHTML = "hello";
